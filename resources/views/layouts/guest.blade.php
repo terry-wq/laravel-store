1dbba6d97419923898d5ef3fcabefaba07cfe4b3
terry-wq <host.free.2024@gmail.com>
2026-05-20 15:57:47 -0600

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,8 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="/assets/app.css">
-        <script type="module" src="/assets/app.js"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         
     </head>
