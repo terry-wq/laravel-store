bb51b62282af3f392ef29a311d78499a20fb819c
terry-wq <host.free.2024@gmail.com>
2026-05-20 16:07:18 -0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        apiPrefix: '',
+        
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
