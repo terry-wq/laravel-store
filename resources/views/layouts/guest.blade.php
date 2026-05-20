505f6e0c5198d80848312f5fd4eab0c3f0d7c2b0
terry-wq <host.free.2024@gmail.com>
2026-05-20 16:21:30 -0600

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
         <!-- Scripts -->
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
 
         
     </head>
