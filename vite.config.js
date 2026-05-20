5bf7ffab4f9d2d39f36e54b71f253dd258c9e0eb
terry-wq <host.free.2024@gmail.com>
2026-05-20 15:46:38 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-});
+
+    build: {
+        outDir: 'public/assets',
+        emptyOutDir: true,
+        manifest: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'app.js',
+                chunkFileNames: 'chunk.js',
+                assetFileNames: 'app.[ext]'
+            }
+        }
+    }
+})
